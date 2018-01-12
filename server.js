1ee051032426169b6f11acfee6ae63b1343cc61f
Evandro Zanatta <hemanstifler@gmail.com>
2018-01-12 01:01:25 -0200

    Ajustando estrutura do projeto para evitar duplicação de post

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ var Twitter = require('twitter');
 
 /* --- Configuracoes do projeto --- */
 
+var db = new sqlite3.Database('posts.db');
+
 var client = new Twitter({
-	consumer_key: process.env.CONSUMER_KEY,
-	consumer_secret: process.env.CONSUMER_SECRET,
-	access_token_key: process.env.ACCESS_TOKEN_KEY,
-	access_token_secret: process.env.ACCESS_TOKEN_SECRET
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
 var options = { method: 'GET',
@@ -43,10 +45,50 @@ db.serialize(function() {
 
 /* --- Funcoes do projeto --- */
 
+// faz o tweet com a noticias
 function postTweet(msg){
+
+	console.log("Inserindo post: " + msg);
+
 	client.post('statuses/update', {status: msg}, function(error, tweet, response) {
 		if (error) {
 			console.log(error);
+		}
+	});
+}
+
+/*
+	verifica se o post ja existe, se nao existir 
+	faz a publicacao de um novo tweet
+*/
+function verifyExist(result, index){
+
+	console.log("index: " + index);
+	console.log("id: "+ result[index]['id']);
+
+	var idTrendsPosts = result[index]['id'];
+	
+	// busca por todas as linhas que possuem o id do post
+	db.all("SELECT id_post FROM posts WHERE id_post = '" + idTrendsPosts + "'", function(err, allRows) {
+		
+		// Se nao existir no banco o id registrado
+		if(allRows.length == 0){
+
+			console.log(idTrendsPosts);
+
+			// Insere no banco de dados
+			db.serialize(function() {
+				console.log('inserindo');
+				db.run("INSERT INTO posts (id_post, time_int) VALUES (?, ?)", [idTrendsPosts, Date.now()]);
+			});
+
+			// Pegar os dados e gera o tweet
+			var hash1 = result[index]['entityNames'][0].replace(/[^a-zA-Z]/g, "");
+			var hash2 = result[index]['entityNames'][1].replace(/[^a-zA-Z]/g, "");
+			var contentPost = result[index]['articles'][0]['articleTitle'] + " #" + hash1 + " #" + hash2 + " " + result[index]['articles'][0]['url'];
+
+			// faz o POST do tweet
+			postTweet(contentPost);
 		}
 	});
 }
@@ -63,32 +105,8 @@ function getTrendsPost(){
 		// percorre todos os posts
 		for (let index = 0; index < 3; index++) {
 
-			var db = new sqlite3.Database('posts.db');
-		
-			var idTrendsPosts = result[index]['id'];
-			
-			// busca por todas as linhas que possuem o id do post
-			db.all("SELECT id_post FROM posts WHERE id_post = '" + idTrendsPosts + "'", function(err, allRows) {
-				
-				// Se nao existir no banco o id registrado
-				if(allRows.length == 0){
-
-					// insere no banco de dados
-					db.serialize(function() {
-						db.run("INSERT INTO posts (id_post, time_int) VALUES (?, ?)", [idTrendsPosts, Date.now()]);
-					});
-
-					// Pegar os dados e gera o tweet
-					var hash1 = result[index]['entityNames'][0].replace(/[^a-zA-Z]/g, "");
-					var hash2 = result[index]['entityNames'][1].replace(/[^a-zA-Z]/g, "");
-					var contentPost = result[index]['articles'][0]['articleTitle'] + " #" + hash1 + " #" + hash2 + " " + result[index]['articles'][0]['url'];
-
-					// faz o POST do tweet
-					postTweet(contentPost);
-				}
-			});
-
-			db.close();
+			// para cada noticia, chama a funcao para verificar se o post existe
+			verifyExist(result, index);	
 		}
 
 	});
